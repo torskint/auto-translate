c940d328ac57d33e000c8155150af93f0f64e288
Dakayao Aristo <da.aristo.y@gmail.com>
2025-09-14 20:13:40 +0100

    Auto-committed on 14/09/2025 20:13:40,63

--- auto-translate.bat
+++ auto-translate.bat
@@ -1,11 +1,11 @@
 @echo off
-echo type "commit" or "update"
+echo Tape "c" pour commit/push ou "q" pour quitter
 
 set current_dir=%cd%
 cd %current_dir%
 
 set GIT_PATH="C:\Program Files\Git\bin\git.exe"
-set BRANCH = "origin master"
+set BRANCH=origin master
 
 :P
 set ACTION=
@@ -13,9 +13,24 @@ set /P ACTION=Action: %=%
 
 if "%ACTION%"=="c" (
 	%GIT_PATH% add -A
-	%GIT_PATH% commit -am "Auto-committed on %date%"
+	%GIT_PATH% commit -am "Auto-committed on %date% %time%"
 	rem %GIT_PATH% pull %BRANCH%
-	%GIT_PATH% push %BRANCH%
+	
+	set TAG_ACTION=
+	set /P TAG_ACTION="Ajouter un tag ? (y/n)": %=%
+
+	if /I "%TAG_ACTION%"=="y" (
+		set TAG_NAME=
+		set /P TAG_NAME="Nom du tag (ex: v1.0.0)": %=%
+
+		%GIT_PATH% push %BRANCH%
+		%GIT_PATH% tag %TAG_NAME%
+		%GIT_PATH% push origin %TAG_NAME%
+		echo ✅ Commit + Push + Tag "%TAG_NAME%" envoyés
+	) else (
+		%GIT_PATH% push %BRANCH%
+		echo ✅ Commit + Push envoyés sans tag
+	)
 )
 
 rem Quitter
